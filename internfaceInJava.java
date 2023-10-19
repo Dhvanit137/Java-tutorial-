b69c74040e370b5153a18247ab99e7d269ec321e
Dhvanit137 <125352682+Dhvanit137@users.noreply.github.com>
2023-10-19 11:12:05 +0530

    JAVA

---
+++ internfaceInJava.java
@@ -0,0 +1,28 @@
+package javademo;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class internfaceInJava {
+
+   public static void main(String args[]) throws IOException {  
+      FileInputStream in = null;
+      FileOutputStream out = null;
+
+      try {
+         in = new FileInputStream("input.txt");
+         out = new FileOutputStream("output.txt");
+         
+         int c;
+         while ((c = in.read()) != -1) {
+            out.write(c);
+         }
+      }finally {
+         if (in != null) {
+            in.close();
+         }
+         if (out != null) {
+            out.close();
+         }
+      }
+   }
+}
